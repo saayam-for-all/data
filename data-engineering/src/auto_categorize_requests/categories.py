a816b273775a1b6c02435c086697c004c38e5bd4
Srilaxmi <kasarlasrilaxmi6@gmail.com>
2026-03-02 21:08:30 -0800

    Issue #100: Auto categorize lambda clean implementation

---
+++ data-engineering/src/auto_categorize_requests/categories.py
@@ -0,0 +1,6 @@
+CATEGORIES = {
+    "food": ["rice", "hungry", "meal", "food", "eat"],
+    "medical": ["doctor", "hospital", "medicine", "injury"],
+    "education": ["school", "books", "fees", "study"],
+    "shelter": ["house", "rent", "home", "stay"]
+}
