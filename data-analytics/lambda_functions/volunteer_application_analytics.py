179e79e03f50073200006d7e16278ee6ff91a0c2
desai_sana <97408625+sana-desai@users.noreply.github.com>
2026-05-29 10:03:33 -0700

    Update volunteer_application_analytics.py

--- data-analytics/lambda_functions/volunteer_application_analytics.py
+++ data-analytics/lambda_functions/volunteer_application_analytics.py
@@ -1,79 +1,113 @@
 import json
 import psycopg2
-import os 
+import boto3
 
-REAL_TABLE_STATE ="virginia_dev_saayam_rdbms.state"
-REAL_TABLE_USERS ="virginia_dev_saayam_rdbms.users"
-REAL_TABLE_VOLUNTEER_DETAILS ="virginia_dev_saayam_rdbms.volunteer_details"
-REAL_TABLE_CITY ="virginia_dev_saayam_rdbms.city"
-REAL_TABLE_USER_SKILLS ="virginia_dev_saayam_rdbms.user_skills"
-REAL_TABLE_VOLUNTEER_LOCATIONS ="virginia_dev_saayam_rdbms.volunteer_locations"
-REAL_TABLE_USER_LOCATIONS ="virginia_dev_saayam_rdbms.user_locations"
-REAL_TABLE_COUNTRY ="virginia_dev_saayam_rdbms.country"
-REAL_TABLE_HELP_CATEGORIES = "virginia_dev_saayam_rdbms.help_categories"
+REAL_TABLE_STATE_VIRGINIA ="virginia_dev_saayam_rdbms.state"
+REAL_TABLE_USERS_VIRGINIA ="virginia_dev_saayam_rdbms.users"
+REAL_TABLE_VOLUNTEER_DETAILS_VIRGINIA ="virginia_dev_saayam_rdbms.volunteer_details"
+REAL_TABLE_CITY_VIRGINIA ="virginia_dev_saayam_rdbms.city"
+REAL_TABLE_USER_SKILL_VIRGINIA ="virginia_dev_saayam_rdbms.user_skills"
+REAL_TABLE_VOLUNTEER_LOCATIONS_VIRGINIA ="virginia_dev_saayam_rdbms.volunteer_locations"
+REAL_TABLE_USER_LOCATIONS_VIRGINIA ="virginia_dev_saayam_rdbms.user_locations"
+REAL_TABLE_COUNTRY_VIRGINIA ="virginia_dev_saayam_rdbms.country"
+REAL_TABLE_HELP_CATEGORIES_VIRGINIA = "virginia_dev_saayam_rdbms.help_categories"
+
+
+REAL_TABLE_STATE_IRELAND ="ireland_dev_saayam_rdbms.state"
+REAL_TABLE_USERS_IRELAND ="ireland_dev_saayam_rdbms.users"
+REAL_TABLE_VOLUNTEER_DETAILS_IRELAND ="ireland_dev_saayam_rdbms.volunteer_details"
+REAL_TABLE_CITY_IRELAND ="ireland_dev_saayam_rdbms.city"
+REAL_TABLE_USER_SKILLS_IRELAND ="ireland_dev_saayam_rdbms.user_skills"
+REAL_TABLE_VOLUNTEER_LOCATIONS_IRELAND ="ireland_dev_saayam_rdbms.volunteer_locations"
+REAL_TABLE_USER_LOCATIONS_IRELAND ="ireland_dev_saayam_rdbms.user_locations"
+REAL_TABLE_COUNTRY_IRELAND ="ireland_dev_saayam_rdbms.country"
+REAL_TABLE_HELP_CATEGORIES_IRELAND = "ireland_dev_saayam_rdbms.help_categories"
 
 def lambda_handler(event, context):
-    conn = None
-    cursor = None
+    conn_V = None
+    cursor_V = None
+    conn_I = None
+    cursor_I = None
 
     safe_response = {
         "volunteer_activity_trend":{
         "new_volunteers": [],
         "active_volunteers": [],
-        "total_volunteers": []}
+        "total_volunteers": []},
+        "volunteers_by_location":[]
     }
-    DB_CONFIG = {
-        "host": os.environ['host'],
-        "port": os.environ['port'],
-        "dbname": os.environ['dbname'],
-        "user": os.environ['user'],
-        "password": os.environ['password']
-    }
+
     try:
         # Connects to database
-        conn = psycopg2.connect(**DB_CONFIG)
-        cursor = conn.cursor()
-        print ("Successfully connected to the databases")
+        VIRGINIA_DB_CONFIG = get_db_config('Virginia')
+        IRELAND_DB_CONFIG = get_db_config('Ireland')
+        conn_V = psycopg2.connect(**VIRGINIA_DB_CONFIG)
+        cursor_V = conn_V.cursor()
+        print("Virginia database connected succcessfully.")
+        conn_I = psycopg2.connect(**IRELAND_DB_CONFIG)
+        cursor_I = conn_I.cursor()
+        print ("Ireland database connected succcessfully.")
 
         country = event.get("country", "All Countries")
         chart_type = event.get("chart_type", "Bar Chart")
         skill = event.get("skill", "All Skills")
+     
+        volunteer_activity_trend_virginia = get_volunteer_activity_trend(cursor_V, REAL_TABLE_USERS_VIRGINIA, REAL_TABLE_VOLUNTEER_DETAILS_VIRGINIA)
+        volunteers_by_location_virginia =  get_volunteers_by_location(cursor_V,REAL_TABLE_USERS_VIRGINIA,REAL_TABLE_VOLUNTEER_DETAILS_VIRGINIA,REAL_TABLE_COUNTRY_VIRGINIA,REAL_TABLE_USER_SKILL_VIRGINIA,REAL_TABLE_HELP_CATEGORIES_VIRGINIA,country,chart_type,skill)
+
+        volunteer_activity_trend_ireland = get_volunteer_activity_trend(cursor_I, REAL_TABLE_USERS_IRELAND, REAL_TABLE_VOLUNTEER_DETAILS_IRELAND)
+        volunteers_by_location_ireland =  get_volunteers_by_location(cursor_I, REAL_TABLE_USERS_IRELAND, REAL_TABLE_VOLUNTEER_DETAILS_IRELAND, REAL_TABLE_COUNTRY_IRELAND, REAL_TABLE_USER_SKILLS_IRELAND, REAL_TABLE_HELP_CATEGORIES_IRELAND,country, chart_type,skill)
 
         response_data = {
-             "volunteer_activity_trend": get_volunteer_activity_trend(cursor),
-             "volunteers_by_location": get_volunteers_by_location(cursor, 
-            country, 
-            chart_type, 
-            skill)
+            "volunteer_activity_trend" : merge_volunteer_activity_trend(volunteer_activity_trend_virginia, volunteer_activity_trend_ireland ),
+            "volunteers_by_location" : merge_volunteer_by_location(volunteers_by_location_virginia, volunteers_by_location_ireland)
         }
 
         return {
-            "statusCode": 200,
-            "body": json.dumps(response_data)
+        "statusCode": 200,
+        "headers":{
+            "Content-Type": "application/json",
+            "Access-Control-Allow-Origin": "*",
+            "Access-Control-Allow-Headers": "Content-Type,Authorization",
+            "Access-Control-Allow-Methods": "GET,POST,OPTIONS"
+        },
+        "body": json.dumps(response_data)
         }
+        
     except Exception as e:
         # Catches an error if connection fails
         print("ERROR:", str(e))
         return {
-            'statusCode': 500,
-            'body': json.dumps(safe_response)
+            "statusCode": 500,
+            "headers":{
+                "Content-Type": "application/json",
+                "Access-Control-Allow-Origin": "*",
+                "Access-Control-Allow-Headers": "Content-Type,Authorization",
+                "Access-Control-Allow-Methods": "GET,POST,OPTIONS"
+           },
+            "body": json.dumps(safe_response)
         }
     finally:
-        if cursor:
-            cursor.close()
-        if conn:
-            conn.close()
-        print("Database connection closed")
+        if cursor_V:
+            cursor_V.close()
+        if conn_V:
+            conn_V.close()
+        print("Virginia Database connection closed")
 
+        if cursor_I:
+            cursor_I.close()
+        if conn_I:
+            conn_I.close()
+        print("Ireland Database connection closed")
    
 
 
-def get_volunteer_activity_trend(cursor):
+def get_volunteer_activity_trend(cursor,users,volunteer_details):
     try: 
         query1 = f"""SELECT TO_CHAR(DATE_TRUNC('month', vd.created_at), 'YYYY-MM') AS month,
         COUNT(DISTINCT u.user_id) AS count 
-        FROM {REAL_TABLE_USERS} u 
-        JOIN {REAL_TABLE_VOLUNTEER_DETAILS} vd ON u.user_id = vd.user_id 
+        FROM {users} u 
+        JOIN {volunteer_details} vd ON u.user_id = vd.user_id 
         WHERE vd.created_at IS NOT NULL 
         GROUP BY 1 
         ORDER BY 1 ASC"""
@@ -85,8 +119,8 @@ def get_volunteer_activity_trend(cursor):
 
         query2 = f""" 
         SELECT TO_CHAR(DATE_TRUNC('month', vd.created_at), 'YYYY-MM') AS month,
-        COUNT(DISTINCT u.user_id) AS count FROM {REAL_TABLE_USERS} u 
-        JOIN {REAL_TABLE_VOLUNTEER_DETAILS} vd ON u.user_id = vd.user_id
+        COUNT(DISTINCT u.user_id) AS count FROM {users} u 
+        JOIN {volunteer_details} vd ON u.user_id = vd.user_id
         WHERE vd.created_at IS NOT NULL
         AND u.user_status_id = 1 
         GROUP BY 1
@@ -100,8 +134,8 @@ def get_volunteer_activity_trend(cursor):
         SELECT month, SUM(count) OVER (ORDER BY month) AS count
         FROM ( SELECT TO_CHAR(DATE_TRUNC('month', vd.created_at), 'YYYY-MM') AS month,
         COUNT(DISTINCT u.user_id) AS count
-        FROM {REAL_TABLE_USERS} u
-        JOIN {REAL_TABLE_VOLUNTEER_DETAILS} vd
+        FROM {users} u
+        JOIN {volunteer_details} vd
         ON u.user_id = vd.user_id
         WHERE vd.created_at IS NOT NULL
         GROUP BY 1 ) sub
@@ -125,18 +159,52 @@ def get_volunteer_activity_trend(cursor):
             "total_volunteers": []
         }
 
-def get_volunteers_by_location(cursor, country='All Countries', chart_type="Bar Chart",skill="All Skills"):
+def merge_monthly_data(list1, list2):
+    merged= {}
+    for row in list1 + list2 : 
+        month = row['month']
+        count = row['count']
+
+        merged[month] = merged.get(month,0) + count
+
+    return [
+        {'month': month, 'count': merged[month]}
+        for month in sorted(merged.keys())] 
+
+def merge_volunteer_activity_trend(volunteer_activity_trend_virginia, volunteer_activity_trend_ireland):
+    return {
+        "new_volunteers": merge_monthly_data(
+            volunteer_activity_trend_virginia.get("new_volunteers", []),
+            volunteer_activity_trend_ireland.get("new_volunteers", [])
+        ),
+        "active_volunteers": merge_monthly_data(
+            volunteer_activity_trend_virginia.get("active_volunteers", []),
+            volunteer_activity_trend_ireland.get("active_volunteers", [])
+        ),
+        "total_volunteers": merge_monthly_data(
+            volunteer_activity_trend_virginia.get("total_volunteers", []),
+            volunteer_activity_trend_ireland.get("total_volunteers", [])
+        )
+    }
+def merge_volunteer_by_location(list1, list2): 
+    merged = {} 
+    for row in list1 + list2:
+        country = row["country"] 
+        count = row["count"] 
+        merged[country] = merged.get(country, 0) + count 
+    return [ {"country": country, "count": merged[country]} for country in sorted(merged.keys()) ]
+
+
+def get_volunteers_by_location( cursor, users, volunteer_details, country_table, user_skills,help_categories,country='All Countries',chart_type="Bar Chart",skill="All Skills"):
     try: 
         query= f"""SELECT
                 COALESCE(c.country_name, 'Unknown') AS country,
                 COUNT(DISTINCT u.user_id) AS count
-            FROM {REAL_TABLE_USERS} u
-            JOIN {REAL_TABLE_VOLUNTEER_DETAILS} vd
+            FROM {users} u
+            JOIN {volunteer_details} vd
                 ON u.user_id = vd.user_id
-            LEFT JOIN {REAL_TABLE_COUNTRY} c
+            LEFT JOIN {country_table} c
                 ON u.country_id = c.country_id
-            LEFT JOIN {REAL_TABLE_STATE} s 
-            ON u.state_id = s.state_id
             WHERE 1=1
             """
         
@@ -150,8 +218,8 @@ def get_volunteers_by_location(cursor, country='All Countries', chart_type="Bar 
         if skill != 'All Skills':
             query += f""" 
             AND EXISTS (SELECT 1 
-            FROM {REAL_TABLE_USER_SKILLS} us JOIN
-            {REAL_TABLE_HELP_CATEGORIES} h ON 
+            FROM {user_skills} us JOIN
+            {help_categories} h ON 
             us.cat_id = h.cat_id 
             WHERE us.user_id = u.user_id 
             AND h.cat_name = %s)""" 
@@ -166,18 +234,50 @@ def get_volunteers_by_location(cursor, country='All Countries', chart_type="Bar 
         cursor.execute(query, params)
         rows = cursor.fetchall()
 
-        return [
-            {"country": row[0], "count": int(row[1])}
-            for row in rows
+        return[
+        {"country": row[0], "count": int(row[1])}
+        for row in rows
         ]
 
     except Exception as e:
         print("Error in get_volunteers_by_location:", str(e))
         return []
 
-    
+def get_db_config(db):
+    ssm = boto3.client("ssm", region_name="us-east-1")
+
+    if db == "Virginia":
+        parameter_name = "/dev/saayam/db/Virginia/Analytics/user"
+    elif db == "Ireland":
+        parameter_name = "/dev/saayam/db/Ireland/Analytics/user"
+    else:
+        raise ValueError("Database must be either Virginia or Ireland")
+
+    response = ssm.get_parameter(
+        Name=parameter_name,
+        WithDecryption=True
+    )
+
+    config = response["Parameter"]["Value"]
+    config_list = [line.strip() for line in config.splitlines()]
+
+    host = config_list[1].split()[1][1:-2]
+    port = int(config_list[5].split()[1][:-1])
+    dbname = config_list[4].split()[2][1:-2]
+    user = config_list[2].split()[1][1:-2]
+    password = config_list[3].split()[1][1:-2]
+
+    return {
+        "host": host,
+        "port": port,
+        "dbname": dbname,
+        "user": user,
+        "password": password
+    }
+
+
 if __name__ == "__main__":
-    test_event = {"skill":"COOKING_HELP"}
+    test_event = {}
     print(lambda_handler(test_event, None))
     
 
